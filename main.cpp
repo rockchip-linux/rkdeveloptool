8aaf80cd1321a852107d974eb431f2a4857ed5b5
martinSusz <119517241+martinSusz@users.noreply.github.com>
2022-12-01 22:56:58 +0200

    Optional reproducibility feature for PACK arg
    Adding new parameter for PACK argument that will trig the reproducibility feature

--- main.cpp
+++ main.cpp
@@ -60,7 +60,7 @@ void usage()
 	printf("ReadFlashInfo:\t\trfi\r\n");
 	printf("ReadChipInfo:\t\trci\r\n");
 	printf("ReadCapability:\t\trcb\r\n");
-	printf("PackBootLoader:\t\tpack\r\n");
+	printf("PackBootLoader:\t\tpack <reproducibly>\r\n");
 	printf("UnpackBootLoader:\tunpack <boot loader>\r\n");
 	printf("TagSPL:\t\t\ttagspl <tag> <U-Boot SPL>\r\n");
 	printf("-------------------------------------------------------\r\n\r\n");
@@ -1390,23 +1390,29 @@ static inline bool getFileSize(const char *path, uint32_t* size) {
 	return true;
 }
 
-static inline rk_time getTime(void) {
+static inline rk_time getTime(bool reproducible) {
 	rk_time rkTime;
-
-	/*** Making creation of rkxx_loader_vx.xx.xxx.bin file reproducible
-	by setting fixed rkTime (in my case the birthday of my daugther),
-	more info https://reproducible-builds.org/
-	
-	struct tm *tm;
-	time_t tt = time(NULL);
-	tm = localtime(&tt); 
-	***/
-	rkTime.year = 2021; // tm->tm_year + 1900;
-	rkTime.month = 12; // tm->tm_mon + 1;
-	rkTime.day = 15; // tm->tm_mday;
-	rkTime.hour = 13; // tm->tm_hour;
-	rkTime.minute = 00; // tm->tm_min;
-	rkTime.second = 00; // tm->tm_sec;
+	if (reproducible) {
+    	/*** Making creation of rkxx_loader_vx.xx.xxx.bin file reproducible
+        by setting fixed rkTime (in this case the birthday of my daugther),
+        more info https://reproducible-builds.org/ ***/
+    		rkTime.year = 2021;
+    		rkTime.month = 12;
+    		rkTime.day = 15;
+    		rkTime.hour = 13;
+    		rkTime.minute = 00;
+    		rkTime.second = 00;
+  	} else {
+    		struct tm *tm;
+    		time_t tt = time(NULL);
+    		tm = localtime(&tt);
+    		rkTime.year = tm->tm_year + 1900;
+    		rkTime.month = tm->tm_mon + 1;
+    		rkTime.day = tm->tm_mday;
+    		rkTime.hour = tm->tm_hour;
+    		rkTime.minute = tm->tm_min;
+    		rkTime.second = tm->tm_sec;
+  	}
 	printf("%d-%d-%d %02d:%02d:%02d\n",
 			rkTime.year, rkTime.month, rkTime.day,
 			rkTime.hour, rkTime.minute, rkTime.second);
@@ -1547,13 +1553,13 @@ end:
 	return chipType;
 }
 
-static inline void getBoothdr(rk_boot_header* hdr) {
+static inline void getBoothdr(rk_boot_header* hdr, bool reproducible) {
 	memset(hdr, 0, sizeof(rk_boot_header));
 	hdr->tag = TAG;
 	hdr->size = sizeof(rk_boot_header);
 	hdr->version = (getBCD(gOpts.major) << 8) | getBCD(gOpts.minor);
 	hdr->mergerVersion = MERGER_VERSION;
-	hdr->releaseTime = getTime();
+	hdr->releaseTime = getTime(reproducible);
 	hdr->chipType = getChipType(gOpts.chip);
 
 	hdr->code471Num = gOpts.code471Num;
@@ -1590,7 +1596,7 @@ end:
 	return crc;
 }
 
-bool mergeBoot(void) {
+bool mergeBoot(bool reproducible) {
 	uint32_t dataOffset;
 	bool ret = false;
 	int i;
@@ -1622,7 +1628,7 @@ bool mergeBoot(void) {
 		goto end;
 	}
 
-	getBoothdr(&hdr);
+	getBoothdr(&hdr, reproducible);
 	printf("Writing header...\n");
 	fwrite(&hdr, 1, sizeof(rk_boot_header), outFile);
 
@@ -3075,7 +3081,7 @@ bool handle_command(int argc, char* argv[], CRKScan *pScan)
 		printf("rkdeveloptool ver %s\r\n", PACKAGE_VERSION);
 		return true;
 	} else if (strcmp(strCmd.c_str(), "PACK") == 0) {//pack boot loader
-		mergeBoot();
+		mergeBoot(argc > 2);
 		return true;
 	} else if (strcmp(strCmd.c_str(), "UNPACK") == 0) {//unpack boot loader
 		string strLoader = argv[2];
